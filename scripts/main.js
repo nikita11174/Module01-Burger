bcc5a77c72beaef0ebd2d29b0104f8c46817ad53
nikita11174 <nikita11174@gmail.com>
2022-12-26 01:36:55 +0300

    18 update

--- scripts/main.js
+++ scripts/main.js
@@ -44,3 +44,16 @@ function scrollFunction() {
     }
 }
 
+// Menu
+
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.top = "-400px";
+}
+
+
+
+
